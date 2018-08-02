58391d6e7e50ae07316e9fb2a02c64227679de56
kbeckman <kbeckman@redfournine.com>
2018-08-02 11:50:43 -0500

    updating spec_helper.rb with cookbook name and target environment options

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+COOKBOOK_NAME = 'bootstrap'.freeze
+
+CHEF_SPEC_OPTS = { platform: 'mac_os_x', version: '10.13' }.freeze
