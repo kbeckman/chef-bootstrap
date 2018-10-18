3f47ac87b641b8a3cd2fb75a58b16878f41e489a
kbeckman <kbeckman@redfournine.com>
2018-10-18 11:56:13 -0500

    ::default modifications, rubocop updates

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,31 +1,23 @@
-#
-# Cookbook:: chef-bootstrap
-# Spec:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# frozen_string_literal: true
 
 require 'spec_helper'
 
-describe 'chef-bootstrap::default' do
+describe 'bootstrap::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:user) { 'jduggan' }
+
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.13') do |node|
+        node.normal[COOKBOOK_NAME]['user'] = user
+      end
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    before do
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
+      RECIPES.each { |recipe| allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with(recipe) }
     end
 
     it 'converges successfully' do
