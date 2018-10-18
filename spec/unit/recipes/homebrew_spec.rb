5f07595281a3d9061608c2f0038dd572b0e1df31
kbeckman <kbeckman@redfournine.com>
2018-10-18 11:54:57 -0500

    renaming ::homebrew, rubocop updates

--- spec/unit/recipes/homebrew_spec.rb
+++ spec/unit/recipes/homebrew_spec.rb
@@ -1,17 +1,21 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-RSpec.describe "#{COOKBOOK_NAME}::_homebrew" do
+RSpec.describe "#{COOKBOOK_NAME}::homebrew" do
   RECIPES       = %w[homebrew homebrew::install_formulas homebrew::install_casks].freeze
-  EXECUTE_CMDS  = { brew_prune:         'brew prune',
-                    brew_cleanup:       'brew cleanup',
-                    brew_cask_cleanup:  'brew cask cleanup' }.freeze
+  EXECUTE_CMDS  = { brew_prune:        'brew prune',
+                    brew_cleanup:      'brew cleanup',
+                    brew_cask_cleanup: 'brew cask cleanup' }.freeze
 
   let(:user) { 'jduggan' }
 
   let(:chef_run) do
-    ChefSpec::SoloRunner.new(CHEF_SPEC_OPTS) do |node|
+    runner = ChefSpec::SoloRunner.new(CHEF_SPEC_OPTS) do |node|
       node.normal[COOKBOOK_NAME]['user'] = user
-    end.converge(described_recipe)
+    end
+
+    runner.converge(described_recipe)
   end
 
   before do
@@ -31,7 +35,7 @@ RSpec.describe "#{COOKBOOK_NAME}::_homebrew" do
 
   describe 'Execute Blocks:' do
     EXECUTE_CMDS.each_pair do |resource, command|
-      it "executes '#{k}'" do
+      it "executes '#{resource}'" do
         expect(chef_run).to run_execute(resource.to_s).with(user: user, command: command)
       end
     end
