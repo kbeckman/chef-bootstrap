09adfb68b40bf6c4c491fb48a6f772eb7e3053ff
kbeckman <kbeckman@redfournine.com>
2018-10-31 22:06:28 -0500

    finishing out ::default

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 include_recipe "#{cookbook_name}::homebrew"
+include_recipe "#{cookbook_name}::oh_my_zsh"
+include_recipe "#{cookbook_name}::homesick"
+
+reboot 'reboot_system' do
+  reason      'End of chef-client run...'
+  delay_mins  0
+  action      :request_reboot
+end
 
 # build_essential 'install_C_compiler' do
 #   compile_time  true
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -3,25 +3,36 @@
 require 'spec_helper'
 
 describe 'bootstrap::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:user) { 'jduggan' }
+  included_recipes = %w[bootstrap::homebrew bootstrap::oh_my_zsh bootstrap::homesick]
 
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.13') do |node|
-        node.normal[COOKBOOK_NAME]['user'] = user
+  let(:user) { 'jduggan' }
+
+  let(:chef_run) do
+    runner = ChefSpec::SoloRunner.new(CHEF_SPEC_OPTS) do |node|
+      node.normal[COOKBOOK_NAME]['user'] = user
+    end
+    runner.converge(described_recipe)
+  end
+
+  before(:each) do
+    allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
+    included_recipes.each { |recipe| allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with(recipe) }
+  end
+
+  describe 'Included Recipes:' do
+    included_recipes.each do |recipe|
+      it "includes '#{recipe}'" do
+        expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with(recipe)
+
+        chef_run
       end
-      runner.converge(described_recipe)
     end
+  end
 
-    before do
-      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
-      RECIPES.each { |recipe| allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with(recipe) }
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+  describe 'reboot[reboot_system]' do
+    it 'executes command' do
+      expect(chef_run).to request_reboot('reboot_system')
+        .with(reason: 'End of chef-client run...', delay_mins: 0)
     end
   end
 end
